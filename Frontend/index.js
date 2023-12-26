70ce272bc1bac3ee5238fbde7c285f2cb6017c9c
Meghna Das <gdyes128@gmail.com>
2023-12-26 22:04:46 +0530

    changes made

--- Frontend/index.js
+++ Frontend/index.js
@@ -12,3 +12,19 @@ login.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var loginButton = document.querySelector('.login-box .clkbtn');
+    if (loginButton) {
+        loginButton.addEventListener('click', function () {
+            location.reload();
+        });
+    }
+    var signupButton = document.querySelector('.signup-box .clkbtn');
+    if (signupButton) {
+        signupButton.addEventListener('click', function () {
+            location.reload();
+        });
+    }
+});
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 1-Login-and-Signup-Page
+TECHNOLOGIES USED :
+1. HTML (Hyper Text Markup Language)
+2. CSS (Cascading Style Sheets)
+3. JS (JavaScript)
